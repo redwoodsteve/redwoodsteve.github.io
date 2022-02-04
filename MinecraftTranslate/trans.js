9f2a01ac7c9221ed56c1bbad4850ac2dec60f833
redwoodsteve <80862755+redwoodsteve@users.noreply.github.com>
2022-02-04 10:51:58 -0800

    Update trans.js

--- MinecraftTranslate/trans.js
+++ MinecraftTranslate/trans.js
@@ -1,10 +1,11 @@
 const trans = document.getElementById(srch)
+const libout = JSON.parse(lib)
 var lib = {
 God["trans":"Notch"]
 Monster["trans":"mob"]
 
 }
 function translate() {
-  document.getElementById('out').innerHTML = lib.trans.trans
+  document.getElementById('out').innerHTML = libout.trans.trans
   
 }
