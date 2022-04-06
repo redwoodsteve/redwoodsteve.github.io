9f474d08205fa0133e5b3523981bdac2191e9af3
redwoodsteve <80862755+redwoodsteve@users.noreply.github.com>
2022-04-06 07:53:34 -0700

    Create elements.js

---
+++ TradingCardDatabase/elements.js
@@ -0,0 +1,23 @@
+//any element has to have an ultra version with "u" at the end. There is no need to hard-code the elements in!
+var elements = {
+    "ruby" : {
+        "name": "Ruby",
+        "value": 100,  
+        "ultra": false
+    },
+    "rubyu" : {
+        "name": "Ruby",
+        "value": 200,
+        "ultra": true
+    },
+    "amythyst" : {
+        "name": "Amythyst",
+        "value": 300,
+        "ultra": false
+    },
+    "amythystu" : {
+        "name": "Amythyst",
+        "value": 400,
+        "ultra": true
+    },
+}
