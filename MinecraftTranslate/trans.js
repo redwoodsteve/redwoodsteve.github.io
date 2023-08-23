00ed04dbbe4609c8c076dc9534ace0525a95a956
redwoodsteve <80862755+redwoodsteve@users.noreply.github.com>
2023-08-22 21:12:53 -0700

    Delete MinecraftTranslate directory

--- MinecraftTranslate/trans.js
+++
@@ -1,8 +0,0 @@
-const trans = document.getElementById(srch).value
-var lib = {
-God["tr":"Notch"]
-Monster["tr":"mob"]
-function translate() {
-  document.getElementById('out').innerHTML = lib[trans]tr
-  
-}
