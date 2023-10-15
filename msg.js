2c3aa039ba5f6a89afe61b8cb76085f7e88e087d
redwoodsteve <chrislanders700@gmail.com>
2023-10-15 09:17:28 -0700

    add a random message to homepage

---
+++ msg.js
@@ -0,0 +1,10 @@
+var msg = [
+    "this is the first ever message",
+    "testing testing 123",
+    "made with 0% beef",
+    "cheborger",
+    "...",
+    "idk what to put here",
+    "this message is a referance to among us",
+    "snickers bars = tasty"
+]
